ab8d86f46048b3702a02f14ebbb28af7a628467f
Derek Kwan <derek.x.kwan@gmail.com>
2020-07-22 15:25:25 -0700

    split up arrays to get around 1000 limitation

--- dd-osctopdstruct.py
+++ dd-osctopdstruct.py
@@ -6,7 +6,7 @@ import sys
 minmax = {"ppg:ir": {"min": 999999, "max": -999999}, "ppg:red": {"min": 999999, "max": -999999}, "ppg:grn": {"min": 999999, "max": -999999}, "eda": {"min": 999999, "max": -999999}, "temp": {"min": 999999, "max": -999999}, "therm": {"min": 999999, "max": -999999}, "humidity": {"min": 999999, "max": -999999}, "acc:x": {"min": 999999, "max": -999999}, "acc:y": {"min": 999999, "max": -999999}, "acc:z": {"min": 999999, "max": -999999}, "gyro:x": {"min": 999999, "max": -999999}, "gyro:y": {"min": 999999, "max": -999999}, "gyro:z":  {"min": 999999, "max": -999999}, "mag:x" : {"min": 999999, "max": -999999}, "mag:y": {"min": 999999, "max": -999999}, "mag:z": {"min": 999999, "max": -999999}}
 
 prev = {"ppg:ir": 0, "ppg:red": 0, "ppg:grn": 0, "eda": 0, "temp": 0, "therm": 0, "humidity": 0, "acc:x": 0, "acc:y": 0, "acc:z": 0, "gyro:x": 0, "gyro:y": 0, "gyro:z":  0, "mag:x" : 0, "mag:y": 0, "mag:z": 0}
-outlines = ["data", "template dancerdata", "float pos", "array oscdata emotibit", "", "template emotibit", "float ppg-ir", "float ppg-red", "float ppg-grn", "float eda", "float temp", "float therm", "float humidity", "float acc-x", "float acc-y", "float acc-z", "float gyro-x", "float gyro-y", "float gyro-z", "float mag-x", "float mag-y", "float mag-z", "", "", "dancerdata 0"]
+outlines = ["data", "template dancerdata", "float pos", "array oscdata emotibit", "", "template emotibit", "float x", "float ppg-ir", "float ppg-red", "float ppg-grn", "float eda", "float temp", "float therm", "float humidity", "float acc-x", "float acc-y", "float acc-z", "float gyro-x", "float gyro-y", "float gyro-z", "float mag-x", "float mag-y", "float mag-z", "", "template moving-cursor", "float x", ""]
 
 filename = sys.argv[1]
 curpath,curfile = os.path.split(filename)
@@ -28,13 +28,21 @@ with open(filename) as f:
                 curVal = functools.reduce(lambda x,y: x+y, curVals)/float(len(curVals))
             else:
                 curVal = curVals[0]
-            cur[curParam] = curVal
             if curParam != "frame": 
+                cur[curParam] = curVal
                 if curVal > minmax[curParam]["max"]:
                     minmax[curParam]["max"] = curVal
                 if curVal < minmax[curParam]["min"]:
                     minmax[curParam]["min"] = curVal
-
+            else:
+                cur[curParam] = curVal - 1 #is frame, make 0 index (instead of i index
+        cur_frame = int(cur["frame"])
+        if cur_frame % 1000 == 0:
+            outlines += [""]
+            if cur_frame == 0:
+                outlines += ["moving-cursor 0"]
+            outlines += ["dancerdata " + str(cur_frame)]
+        retStr += str(cur_frame) + " "
         for idx,param in enumerate([*prev]):
             if param not in [*cur]:
                retStr += str(prev[param])
