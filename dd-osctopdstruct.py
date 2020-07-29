7e8b383cba8fdb6f669c1e0a032d061d2d48b67f
Derek Kwan <derek.x.kwan@gmail.com>
2020-07-29 14:37:26 -0700

    adjust osc address, add data play

--- dd-osctopdstruct.py
+++ dd-osctopdstruct.py
@@ -6,7 +6,7 @@ import sys
 minmax = {"ppg:ir": {"min": 999999, "max": -999999}, "ppg:red": {"min": 999999, "max": -999999}, "ppg:grn": {"min": 999999, "max": -999999}, "eda": {"min": 999999, "max": -999999}, "temp": {"min": 999999, "max": -999999}, "therm": {"min": 999999, "max": -999999}, "humidity": {"min": 999999, "max": -999999}, "acc:x": {"min": 999999, "max": -999999}, "acc:y": {"min": 999999, "max": -999999}, "acc:z": {"min": 999999, "max": -999999}, "gyro:x": {"min": 999999, "max": -999999}, "gyro:y": {"min": 999999, "max": -999999}, "gyro:z":  {"min": 999999, "max": -999999}, "mag:x" : {"min": 999999, "max": -999999}, "mag:y": {"min": 999999, "max": -999999}, "mag:z": {"min": 999999, "max": -999999}}
 
 prev = {"ppg:ir": 0, "ppg:red": 0, "ppg:grn": 0, "eda": 0, "temp": 0, "therm": 0, "humidity": 0, "acc:x": 0, "acc:y": 0, "acc:z": 0, "gyro:x": 0, "gyro:y": 0, "gyro:z":  0, "mag:x" : 0, "mag:y": 0, "mag:z": 0}
-outlines = ["data", "template dancerdata", "float pos", "array oscdata emotibit", "", "template emotibit", "float x", "float ppg-ir", "float ppg-red", "float ppg-grn", "float eda", "float temp", "float therm", "float humidity", "float acc-x", "float acc-y", "float acc-z", "float gyro-x", "float gyro-y", "float gyro-z", "float mag-x", "float mag-y", "float mag-z", "", "template moving-cursor", "float x", ""]
+outlines = ["data", "template dancerdata", "float pos", "float numframes", "array oscdata emotibit", "", "template emotibit", "float x", "float ppg-ir", "float ppg-red", "float ppg-grn", "float eda", "float temp", "float therm", "float humidity", "float acc-x", "float acc-y", "float acc-z", "float gyro-x", "float gyro-y", "float gyro-z", "float mag-x", "float mag-y", "float mag-z", "", "template moving-cursor", "float x", ""]
 
 filename = sys.argv[1]
 curpath,curfile = os.path.split(filename)
@@ -14,7 +14,9 @@ basename,ext = os.path.splitext(curfile)
 outfile = os.path.join(curpath, basename + ".pdstruct")
 
 with open(filename) as f:
-    for line in f:
+    curlines = f.read().splitlines()
+    totlines = len(curlines)
+    for line in curlines:
         databits = re.findall("/[^\s]*[^\s][^/]*", line.strip()) # splits into addresses and values
         cur = {}
         retStr = ""
@@ -41,7 +43,7 @@ with open(filename) as f:
             outlines += [""]
             if cur_frame == 0:
                 outlines += ["moving-cursor 0"]
-            outlines += ["dancerdata " + str(cur_frame)]
+            outlines += ["dancerdata " + str(cur_frame) + " " + str(totlines)]
         retStr += str(cur_frame) + " "
         for idx,param in enumerate([*prev]):
             if param not in [*cur]:
